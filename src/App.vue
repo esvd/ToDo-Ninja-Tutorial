84c69680478bd089d2d520bb87aeb6533550354c
Vinicius Escame Dos Santos <x387283@SF.Prefeitura.SP.GOV.BR>
2019-11-06 15:53:38 -0300

    changed NavBar.vue from Home.vue (only) to App.vue (all views)

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
   <v-app>
     <v-content>
       <router-view></router-view>
+      <NavBar />
     </v-content>
   </v-app>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
 export default {
   name: "App",
-  data: () => ({})
+  components: {
+    NavBar
+  }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <NavBar />
+    <!-- <NavBar /> -->
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
-export default {
-  components: {
-    NavBar
-  }
-};
+// import NavBar from "@/components/NavBar.vue";
+// export default {
+//   components: {
+//     NavBar
+//   }
+// };
+//
 </script>
