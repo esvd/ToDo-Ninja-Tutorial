bac8e0ccf7267e70173314fc074efcfc74155952
Vinicius Escame Dos Santos <x387283@SF.Prefeitura.SP.GOV.BR>
2019-11-06 14:28:00 -0300

    implementing home NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <v-app-bar color="grey lighten-5" flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">todo</span>
+        <span>ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer class="drawer" v-model="drawer" app></v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true
+    };
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="home">
-    <h1>This is the home page</h1>
+  <div>
+    <NavBar />
   </div>
 </template>
 
 <script>
-export default {};
+import NavBar from "@/components/NavBar.vue";
+export default {
+  components: {
+    NavBar
+  }
+};
 </script>
