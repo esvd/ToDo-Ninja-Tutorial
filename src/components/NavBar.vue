a84a251381ce11a3e16e6464294f4853ce2be7c3
Vinicius Escame Dos Santos <x387283@SF.Prefeitura.SP.GOV.BR>
2019-11-25 16:17:14 -0300

    tutorial 22 - menus

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,25 @@
 <template>
   <nav>
     <v-app-bar color="grey lighten-5" flat>
-      <v-app-bar-nav-icon
-        class="grey--text"
-        @click="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="hidden-sm-and-down text-uppercase grey--text">
         <span class="font-weight-light">todo</span>
         <span>ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-menu bottom offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on" color="grey">
+            <v-icon>expand_more</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" @click="on" router :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <v-btn text color="grey">
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
@@ -22,27 +32,17 @@
           <v-avatar size="100">
             <img src="/avatar-1.png" />
           </v-avatar>
-          <p class="white--text subheading">
-            The Net Ninja
-          </p>
+          <p class="white--text subheading">The Net Ninja</p>
         </v-col>
       </v-row>
       <v-list flat>
         <v-list-item-group>
-          <v-list-item
-            v-for="(link, i) in links"
-            :key="i"
-            router
-            :to="link.route"
-          >
+          <v-list-item v-for="(link, i) in links" :key="i" router :to="link.route">
             <v-list-item-icon>
               <v-icon class="white--text" v-text="link.icon"></v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title
-                class="white--text"
-                v-text="link.text"
-              ></v-list-item-title>
+              <v-list-item-title class="white--text" v-text="link.text"></v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
